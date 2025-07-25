49709ed990b1a7dd017c2914d6ba41910fc1726a
Farhad Azish <farhad.azish@gmail.com>
2025-07-25 21:50:07 +0330

    Add minimal footer component

---
+++ tests/components/footer.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import Footer from '../../components/TheFooter.vue'
+
+describe('TheFooter component', () => {
+  it('is defined', () => {
+    expect(Footer).toBeTruthy()
+  })
+})
